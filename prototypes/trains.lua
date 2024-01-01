74198acdc140059563a1d9cf81398a84360c15b8
PreLeyZero <52599424+PreLeyZero@users.noreply.github.com>
2024-01-01 23:01:04 +0100

    added wagon and shadow seperation

--- prototypes/trains.lua
+++ prototypes/trains.lua
@@ -15,6 +15,16 @@ data:extend({
         place_result = "ei_em-locomotive-temp",
         stack_size = 50
     },
+	{
+        name = "ei_em-wagon-temp",
+        type = "item",
+        icon = ei_trains_item_path.."em-wagon-temp.png",
+        icon_size = 64,
+        subgroup = "transport",
+        order = "x2",
+        place_result = "ei_em-wagon-temp",
+        stack_size = 50
+    },
 })
 
 --====================================================================================================
@@ -33,6 +43,18 @@ data:extend({
         enabled = true,
         always_show_made_in = true,
         main_product = "ei_em-locomotive-temp",
+    },
+	{
+        name = "ei_em-wagon-temp",
+        type = "recipe",
+        category = "crafting",
+        energy_required = 1,
+        ingredients = {},
+        result = "ei_em-wagon-temp",
+        result_count = 1,
+        enabled = true,
+        always_show_made_in = true,
+        main_product = "ei_em-wagon-temp",
     },
 })
 
@@ -261,5 +283,91 @@ data:extend({
 		open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
 		close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
 		sound_minimum_speed = 0.2;
+	},
+	{
+		type = "cargo-wagon",
+		name = "ei_em-wagon-temp",
+		icon = ei_trains_item_path.."em-wagon-temp.png",
+        icon_size = 32,
+		flags = {"placeable-neutral", "player-creation", "placeable-off-grid", },
+		inventory_size = 20,
+		minable = {
+            mining_time = 1,
+            result = "ei_em-wagon-temp"
+        },
+		mined_sound = {filename = "__core__/sound/deconstruct-medium.ogg"},
+		max_health = 600,
+		corpse = "medium-remnants",
+		dying_explosion = "medium-explosion",
+		
+		collision_box = {{-0.6, -2.4}, {0.6, 2.4}},
+		selection_box = {{-1.0, -2.7}, {1, 3.3}},		
+		connection_distance = 3, joint_distance = 4,
+		
+		weight = 1000,
+		max_speed = 1.2,
+		braking_force = 2,
+		friction_force = 0.0015,
+		air_resistance = 0.002,
+		energy_per_hit_point = 5,    
+		resistances =
+		{
+			{type = "fire", decrease = 15, percent = 50 },
+			{type = "physical", decrease = 15, percent = 30 },
+			{type = "impact",decrease = 50,percent = 60},
+			{type = "explosion",decrease = 15,percent = 30},
+			{type = "acid",decrease = 10,percent = 20}
+		},
+		vertical_selection_shift = -0.8,
+		--back_light = rolling_stock_back_light(),
+		--stand_by_light = rolling_stock_stand_by_light(),
+		pictures =
+		{
+			priority = "very-low",
+			width = 512,
+			height = 512,
+			direction_count = 128,
+			filenames =
+			{
+				ei_trains_entity_path.."em-wagon-temp_1.png",
+				ei_trains_entity_path.."em-wagon-temp_2.png"
+			},
+			line_length = 8,
+			lines_per_file = 8,
+		    shift = {0, -0.5},
+			scale = 0.58,
+			
+		},
+		minimap_representation = {
+			filename = "__base__/graphics/entity/cargo-wagon/cargo-wagon-minimap-representation.png",
+			flags = {"icon"},
+			size = {20, 40},
+			scale = 0.5
+		},
+		selected_minimap_representation = {
+			filename = "__base__/graphics/entity/cargo-wagon/cargo-wagon-selected-minimap-representation.png",
+			flags = {"icon"},
+			size = {20, 40},
+			scale = 0.5
+		},
+
+		wheels = standard_train_wheels,
+		rail_category = "regular",
+		drive_over_tie_trigger = drive_over_tie(),
+		tie_distance = 50,
+		working_sound =
+		{
+			sound =
+			{
+				filename = "__base__/sound/train-wheels.ogg",
+				volume = 0.5
+			},
+			match_volume_to_activity = true,
+		},
+		crash_trigger = crash_trigger(),
+		open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+		close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+		sound_minimum_speed = 0.5;
+		vehicle_impact_sound =  { filename = "__base__/sound/car-wood-impact.ogg", volume = 1.0 },
 	},
 })
